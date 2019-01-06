48750af6d595f795bf2d7fd8b73a2821fd68a541
carlosgonz <45316884+GNUuser@users.noreply.github.com>
2019-01-06 22:50:13 +0000

    Update snipek-es.ts

--- sailfish/translations/snipek-es.ts
+++ sailfish/translations/snipek-es.ts
@@ -1,37 +1,37 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="es" sourcelanguage="en">
+<TS version="2.1" language="es" sourcelanguage="es_GT">
 <context>
     <name>AboutPage</name>
     <message>
         <location filename="../qml/AboutPage.qml" line="26"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Acerca de</translation>
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="42"/>
         <source>Version %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Versión %1</translation>
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="52"/>
         <source>Project website</source>
-        <translation type="unfinished"></translation>
+        <translation>Fuente</translation>
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="58"/>
         <source>Authors</source>
-        <translation type="unfinished"></translation>
+        <translation>Autores</translation>
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="72"/>
         <source>%1 is developed as an open source project under %2.</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 es desarrollado como un proyecto de código abierto bajo %2.</translation>
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="78"/>
         <source>Libraries</source>
-        <translation type="unfinished"></translation>
+        <translation>Librerías</translation>
     </message>
 </context>
 <context>
@@ -39,47 +39,47 @@
     <message>
         <location filename="../qml/FirstPage.qml" line="21"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Acerca de</translation>
     </message>
     <message>
         <location filename="../qml/FirstPage.qml" line="26"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajustes</translation>
     </message>
     <message>
         <location filename="../qml/FirstPage.qml" line="26"/>
         <source>Configure</source>
-        <translation type="unfinished"></translation>
+        <translation>Configurar</translation>
     </message>
     <message>
         <location filename="../qml/FirstPage.qml" line="32"/>
         <source>Disconnect</source>
-        <translation type="unfinished"></translation>
+        <translation>Desconectar</translation>
     </message>
     <message>
         <location filename="../qml/FirstPage.qml" line="32"/>
         <source>Connect</source>
-        <translation type="unfinished"></translation>
+        <translation>Conectar</translation>
     </message>
     <message>
         <location filename="../qml/FirstPage.qml" line="58"/>
         <source>Listening</source>
-        <translation type="unfinished"></translation>
+        <translation>Escuchando</translation>
     </message>
     <message>
         <location filename="../qml/FirstPage.qml" line="59"/>
         <source>Idle</source>
-        <translation type="unfinished"></translation>
+        <translation>Inactivo</translation>
     </message>
     <message>
         <location filename="../qml/FirstPage.qml" line="60"/>
         <source>Disconnected</source>
-        <translation type="unfinished"></translation>
+        <translation>Desconectado</translation>
     </message>
     <message>
         <location filename="../qml/FirstPage.qml" line="65"/>
         <source>Not configured</source>
-        <translation type="unfinished"></translation>
+        <translation>No configurado</translation>
     </message>
 </context>
 <context>
@@ -87,37 +87,37 @@
     <message>
         <location filename="../qml/SettingsPage.qml" line="26"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajustes</translation>
     </message>
     <message>
         <location filename="../qml/SettingsPage.qml" line="32"/>
         <source>Enter Snips MQTT broker IP address</source>
-        <translation type="unfinished"></translation>
+        <translation>Entrar dirección IP del agente MQTT de Snips</translation>
     </message>
     <message>
         <location filename="../qml/SettingsPage.qml" line="33"/>
         <source>Snips MQTT broker address</source>
-        <translation type="unfinished"></translation>
+        <translation>Dirección del agente Snips MQTT</translation>
     </message>
     <message>
         <location filename="../qml/SettingsPage.qml" line="48"/>
         <source>Enter Snips MQTT broker port number</source>
-        <translation type="unfinished"></translation>
+        <translation>Entrar el puerto del agente Snips MQTT</translation>
     </message>
     <message>
         <location filename="../qml/SettingsPage.qml" line="49"/>
         <source>Snips MQTT broker port</source>
-        <translation type="unfinished"></translation>
+        <translation>Puerto del agente Snips MQTT</translation>
     </message>
     <message>
         <location filename="../qml/SettingsPage.qml" line="64"/>
         <source>Enter site ID</source>
-        <translation type="unfinished"></translation>
+        <translation>Entrar identificación del sitio</translation>
     </message>
     <message>
         <location filename="../qml/SettingsPage.qml" line="65"/>
         <source>Site ID</source>
-        <translation type="unfinished"></translation>
+        <translation>Identificación del sitio</translation>
     </message>
 </context>
 <context>
@@ -125,17 +125,17 @@
     <message>
         <location filename="../qml/main.qml" line="34"/>
         <source>Cannot connect because Snips MQTT broker IP address is not defined</source>
-        <translation type="unfinished"></translation>
+        <translation>No se puede conectar porque la dirección IP del agente MQTT de Snips no está hecha</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="37"/>
         <source>Connection problem</source>
-        <translation type="unfinished"></translation>
+        <translation>Problema de conexión</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="40"/>
         <source>Whoops, something went wrong</source>
-        <translation type="unfinished"></translation>
+        <translation>Vaya, algo salió mal</translation>
     </message>
 </context>
 </TS>
