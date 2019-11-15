acd1c2b1b44f923a86df9f07427dcec20039f9c9
Muki <michal@mkiol.net>
2019-11-15 22:43:07 +0100

    Tanslation source file update

--- sailfish/translations/snipek-es.ts
+++ sailfish/translations/snipek-es.ts
@@ -4,60 +4,208 @@
 <context>
     <name>AboutPage</name>
     <message>
-        <location filename="../qml/AboutPage.qml" line="26"/>
+        <location filename="../qml/AboutPage.qml" line="28"/>
         <source>About</source>
         <translation>Acerca de</translation>
     </message>
     <message>
-        <location filename="../qml/AboutPage.qml" line="42"/>
+        <location filename="../qml/AboutPage.qml" line="44"/>
         <source>Version %1</source>
         <translation>Versión %1</translation>
     </message>
     <message>
-        <location filename="../qml/AboutPage.qml" line="52"/>
+        <location filename="../qml/AboutPage.qml" line="48"/>
+        <source>Changelog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="54"/>
         <source>Project website</source>
         <translation>Fuente</translation>
     </message>
     <message>
-        <location filename="../qml/AboutPage.qml" line="58"/>
+        <location filename="../qml/AboutPage.qml" line="60"/>
         <source>Authors</source>
         <translation>Autores</translation>
     </message>
     <message>
-        <location filename="../qml/AboutPage.qml" line="72"/>
+        <location filename="../qml/AboutPage.qml" line="74"/>
         <source>%1 is developed as an open source project under %2.</source>
         <translation>%1 es desarrollado como un proyecto de código abierto bajo %2.</translation>
     </message>
     <message>
-        <location filename="../qml/AboutPage.qml" line="78"/>
+        <location filename="../qml/AboutPage.qml" line="80"/>
         <source>Libraries</source>
         <translation>Librerías</translation>
     </message>
 </context>
 <context>
+    <name>CallHistorySkill</name>
+    <message>
+        <location filename="../../core/callhistoryskill.cpp" line="31"/>
+        <source>Call history</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../core/callhistoryskill.cpp" line="59"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../core/callhistoryskill.cpp" line="122"/>
+        <source>Yesterday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../core/callhistoryskill.cpp" line="155"/>
+        <location filename="../../core/callhistoryskill.cpp" line="217"/>
+        <source>Continue?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../core/callhistoryskill.cpp" line="193"/>
+        <source>You didn&apos;t have any missed calls today.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../core/callhistoryskill.cpp" line="195"/>
+        <source>You didn&apos;t have any missed calls since %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../core/callhistoryskill.cpp" line="198"/>
+        <source>You didn&apos;t have any calls today.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../core/callhistoryskill.cpp" line="200"/>
+        <source>You didn&apos;t have any calls since %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../../core/callhistoryskill.cpp" line="210"/>
+        <source>You had %n call(s) today.</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../../core/callhistoryskill.cpp" line="212"/>
+        <source>You had %n call(s) since %1.</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../../core/callhistoryskill.cpp" line="205"/>
+        <source>You had %n missed call(s) today.</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../../core/callhistoryskill.cpp" line="159"/>
+        <source>That was the last call event.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../../core/callhistoryskill.cpp" line="207"/>
+        <source>You had %n missed call(s) since %1.</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../../core/callhistoryskill.cpp" line="127"/>
+        <source>Missed call from %2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../core/callhistoryskill.cpp" line="26"/>
+        <source>callhistory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../core/callhistoryskill.cpp" line="36"/>
+        <source>Reads events from call history e.g. all missed calls.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../core/callhistoryskill.cpp" line="129"/>
+        <source>Incoming call from %2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../core/callhistoryskill.cpp" line="131"/>
+        <source>Outgoing call to %2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ChangelogPage</name>
+    <message>
+        <location filename="../qml/ChangelogPage.qml" line="28"/>
+        <source>Changelog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ChangelogPage.qml" line="32"/>
+        <source>Version %1</source>
+        <translation type="unfinished">Versión %1</translation>
+    </message>
+</context>
+<context>
+    <name>DateTimeSkill</name>
+    <message>
+        <location filename="../../core/datetimeskill.cpp" line="25"/>
+        <source>datetime</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../core/datetimeskill.cpp" line="30"/>
+        <source>Date and Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../core/datetimeskill.cpp" line="35"/>
+        <source>Reads current time or date.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../core/datetimeskill.cpp" line="47"/>
+        <location filename="../../core/datetimeskill.cpp" line="51"/>
+        <source>It is %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>FirstPage</name>
     <message>
-        <location filename="../qml/FirstPage.qml" line="21"/>
+        <location filename="../qml/FirstPage.qml" line="23"/>
         <source>About</source>
         <translation>Acerca de</translation>
     </message>
     <message>
-        <location filename="../qml/FirstPage.qml" line="26"/>
+        <location filename="../qml/FirstPage.qml" line="28"/>
         <source>Settings</source>
         <translation>Ajustes</translation>
     </message>
     <message>
-        <location filename="../qml/FirstPage.qml" line="26"/>
+        <location filename="../qml/FirstPage.qml" line="28"/>
         <source>Configure</source>
         <translation>Configurar</translation>
     </message>
     <message>
-        <location filename="../qml/FirstPage.qml" line="32"/>
+        <location filename="../qml/FirstPage.qml" line="34"/>
         <source>Disconnect</source>
         <translation>Desconectar</translation>
     </message>
     <message>
-        <location filename="../qml/FirstPage.qml" line="32"/>
+        <location filename="../qml/FirstPage.qml" line="34"/>
         <source>Connect</source>
         <translation>Conectar</translation>
     </message>
@@ -68,16 +216,30 @@
     </message>
     <message>
         <location filename="../qml/FirstPage.qml" line="59"/>
-        <source>Idle</source>
-        <translation>Inactivo</translation>
+        <source>Say wake word or tap</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/FirstPage.qml" line="61"/>
+        <source>Say wake word</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/FirstPage.qml" line="60"/>
+        <source>Tap to wake up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Idle</source>
+        <translation type="vanished">Inactivo</translation>
+    </message>
+    <message>
+        <location filename="../qml/FirstPage.qml" line="62"/>
         <source>Disconnected</source>
         <translation>Desconectado</translation>
     </message>
     <message>
-        <location filename="../qml/FirstPage.qml" line="65"/>
+        <location filename="../qml/FirstPage.qml" line="67"/>
         <source>Not configured</source>
         <translation>No configurado</translation>
     </message>
@@ -85,39 +247,129 @@
 <context>
     <name>SettingsPage</name>
     <message>
-        <location filename="../qml/SettingsPage.qml" line="26"/>
+        <location filename="../qml/SettingsPage.qml" line="28"/>
         <source>Settings</source>
         <translation>Ajustes</translation>
     </message>
     <message>
-        <location filename="../qml/SettingsPage.qml" line="32"/>
         <source>Enter Snips MQTT broker IP address</source>
-        <translation>Entrar dirección IP del agente MQTT de Snips</translation>
+        <translation type="vanished">Entrar dirección IP del agente MQTT de Snips</translation>
     </message>
     <message>
-        <location filename="../qml/SettingsPage.qml" line="33"/>
         <source>Snips MQTT broker address</source>
-        <translation>Dirección del agente Snips MQTT</translation>
+        <translation type="vanished">Dirección del agente Snips MQTT</translation>
+    </message>
+    <message>
+        <source>Enter Snips MQTT broker port number</source>
+        <translation type="vanished">Entrar el puerto del agente Snips MQTT</translation>
+    </message>
+    <message>
+        <source>Snips MQTT broker port</source>
+        <translation type="vanished">Puerto del agente Snips MQTT</translation>
+    </message>
+    <message>
+        <source>Enter site ID</source>
+        <translation type="vanished">Entrar identificación del sitio</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="153"/>
+        <source>Site ID</source>
+        <translation>Identificación del sitio</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="34"/>
+        <source>Audio feedback</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="35"/>
+        <source>Enables notification sound when voice assistant is activated (e.g. a wake word is detected).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="43"/>
+        <source>Wake-up method</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="44"/>
+        <source>The way you can activate the voice assistant.</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/SettingsPage.qml" line="48"/>
-        <source>Enter Snips MQTT broker port number</source>
-        <translation>Entrar el puerto del agente Snips MQTT</translation>
+        <source>Wake word or tap gesture</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/SettingsPage.qml" line="49"/>
-        <source>Snips MQTT broker port</source>
-        <translation>Puerto del agente Snips MQTT</translation>
+        <source>Tap gesture only</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/SettingsPage.qml" line="64"/>
-        <source>Enter site ID</source>
-        <translation>Entrar identificación del sitio</translation>
+        <location filename="../qml/SettingsPage.qml" line="50"/>
+        <source>Wake word only</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/SettingsPage.qml" line="65"/>
-        <source>Site ID</source>
-        <translation>Identificación del sitio</translation>
+        <location filename="../qml/SettingsPage.qml" line="60"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="61"/>
+        <source>Language used for UI and built-in skills. Remember to install Snips assistant that supports selected language as well.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="81"/>
+        <source>Auto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="114"/>
+        <source>Snips configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="120"/>
+        <source>Enter MQTT IP address (e.g. 192.168.1.5)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="121"/>
+        <source>IP address for MQTT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="136"/>
+        <source>Enter MQTT port number (e.g. 1883)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="137"/>
+        <source>MQTT port number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="152"/>
+        <source>Enter unique site ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="167"/>
+        <source>Enter namespace for built-in skills (default is &quot;%1&quot;)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="168"/>
+        <source>Namespace for built-in skills</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="180"/>
+        <source>Skills</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
