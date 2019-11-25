e81ee6f83f1844798e9716825c34d0db62c0ab56
Muki <michal@mkiol.net>
2019-11-25 20:23:19 +0100

    Scripts for Snips installation on SFOS

---
+++ sailfish/snips/snips_download.sh
@@ -0,0 +1,655 @@
+#!/usr/bin/env bash
+
+# Script that automates Snips binaries download to Sailfish OS
+# Copyright (c) Michal Kosciesza <michal@mkiol.net>
+
+VERSION=1.0.0
+
+INSTALL_SNIPS=1
+INSTALL_DEBIAN_MAIN=1
+INSTALL_DEBIAN_NONFREE=1
+INSTALL_ASSISTANT=1
+PATCH_PICO2WAVE=1
+KEEP_PACKAGES_FILE=0
+KEEP_DEBS=0
+KEEP_ASSISTANT=0
+CHECK_ONLY=0
+REUSE_DEB_FILES=0
+ARCH=armhf # Snips provides only binaries for armhf and amd64
+
+SNIPS_DIR_DEFAULT=/home/nemo/.cache/harbour-snipek/harbour-snipek/snips
+#SNIPS_REPO_ROOT=https://debian.snips.ai/stretch
+SNIPS_REPO_ROOT=https://raspbian.snips.ai/stretch
+SNIPS_DIST=stable
+DEBIAN_REPO_ROOT=http://ftp.debian.org/debian
+DEBIAN_DIST=stretch
+SNIPEK_ASSISTANT=https://github.com/mkiol/Snipek/raw/master/assistant/assistant_proj_BAYAr2l4k5z.zip
+
+PICO2WAVE_PATCH="\
+QlNESUZGNDA/AAAAAAAAAC4AAAAAAAAAgCYAAAAAAABCWmg5MUFZJlNZh5qAJwAAGWhAeTEAAkhA\
+QAAgACGmoZkjQgGgCXNOnSAEMwiAb7rZ8sCv4u5IpwoSEPNQBOBCWmg5MUFZJlNZuZq0BwAAE0Ag\
+wAAACAAIIAAwzAUpplGxUeLuSKcKEhczVoDgQlpoOTFBWSZTWWLU0qEAAALQAFAAAAEgACEmQZiQ\
+uLuSKcKEgxamlQg="
+
+########################
+
+work_dir=$(pwd) # current working dir
+declare -a pkgs_to_install_names
+declare -a pkgs_to_install_vers
+declare -a files_to_install
+
+print() {
+  echo "$1"
+}
+
+print_error() {
+  echo "$1" 1>&2
+}
+
+usage() {
+  print "Usage: $0 [OPTION]..."
+  print "Download essential Snips binaries."
+  print ""
+  print "Options:"
+  print "  -d <DIR>     directory where files should be downloaded to (default is $SNIPS_DIR_DEFAULT)"
+  #print "  -a <ARCH>    define cpu architecture (armhf, i386 or amd64)"
+  print "  -c           instead new download, check if all needed files are present"
+  print "  -k           keep deb files"
+  print "  -r           instead download use existing deb packages"
+  print "  -h           display this help and exit"
+  print "  -V           output version information and exit"
+}
+
+exit_abnormal() {
+  local txt="$1"
+  local show_usage="$2"
+  if [ -n "$txt" ]; then
+    print_error "$1"
+  fi
+  if [ -n "$show_usage" ]; then
+    usage
+  fi
+  exit 1
+}
+
+download_and_extract_assistant() {
+  print "Downloading assistant file: $SNIPEK_ASSISTANT"
+  
+  local assistant_archive="$SNIPS_DIR/assistant.zip"
+  local assistant_dir="$SNIPS_DIR/assistant"
+
+  if [ -f "$assistant_archive" ]; then
+    print "No need to download assistant file because it exists."
+  else
+    wget --output-document="$assistant_archive" "$SNIPEK_ASSISTANT"
+
+    if ! [ -f "$assistant_archive" ]; then
+      print_error "Error: Cannot download assistant file."
+      return 1
+    fi
+  fi
+  
+  unzip -o "$assistant_archive" -d "$SNIPS_DIR"
+  
+  if [ $? -ne 0 ] || [ ! -d "$assistant_dir" ]; then
+    print_error "Error: Cannot extract assistant file."
+    
+    if [ $KEEP_ASSISTANT -eq 0 ] && [ -f "$assistant_archive" ]; then
+      print "Deleting file: $assistant_archive"
+      rm "$assistant_archive"
+    fi
+    
+    return 1
+  fi
+  
+  if [ $KEEP_ASSISTANT -eq 0 ] && [ -f "$assistant_archive" ]; then
+    print "Deleting file: $assistant_archive"
+    rm "$assistant_archive"
+  fi
+}
+
+download_and_extract_pkgs() {
+  local repo_root=($1)
+  local package_file_url=($2)
+  
+  if [ ${#pkgs_to_install_names[*]} -eq 0 ]; then
+    print_error "No packages to install."
+    return 1
+  fi
+
+  # download debian repo Package file
+
+  print "Downloading repo Packages file: $package_file_url"
+  
+  local packages_archive="$SNIPS_DIR/Packages.gz"
+  local packages_file="$SNIPS_DIR/Packages"
+  
+  wget --output-document="$packages_archive" $package_file_url
+
+  if ! [ -f "$packages_archive" ]; then
+    print_error "Error: Cannot download repo Packages file."
+    return 1
+  fi
+  
+  gzip -d -f "$packages_archive"
+  
+  if [ $? -ne 0 ] || [ ! -f "$packages_file" ]; then
+    print_error "Error: Cannot extract Packages file."
+    return 1
+  fi
+
+  pkg_names=( $( sed -r 's/^Package: (.*)$/\1/g;t;d' "$packages_file" ) )
+  pkg_vers=( $( sed -r 's/^Version: (.*)$/\1/g;t;d' "$packages_file" ) )
+  pkg_fnames=( $( sed -r 's/^Filename: (.*)$/\1/g;t;d' "$packages_file" ) )
+  
+  if [ $KEEP_PACKAGES_FILE -eq 0 ]; then
+    echo "Deleting file: $packages_file"
+    rm "$packages_file"
+  fi
+
+  # find packages for install
+
+  declare -a found_pkg_names
+  declare -a found_pkg_idx # idx of found pkg in pkg_names
+
+  print "Needed packages:"
+  for idx in "${!pkgs_to_install_names[@]}"
+  do
+    echo " ${pkgs_to_install_names[$idx]} (${pkgs_to_install_vers[$idx]})"
+  done
+
+  print "Finding needed packages in the repo."
+  
+  for idx in "${!pkgs_to_install_names[@]}"
+  do
+    local needed_name="${pkgs_to_install_names[$idx]}"
+    local needed_ver="${pkgs_to_install_vers[$idx]}"
+    print "Searching for $needed_name ($needed_ver)..."
+  
+    for (( i=0; i<=$(( ${#pkg_names[*]} -1 )); i++ ))
+    do
+      if [ "${pkg_names[$i]}" == "$needed_name" ] && [[ "${pkg_vers[$i]}" == "$needed_ver"* ]]; then
+          found_pkg_names+=(${pkg_names[$i]})
+          found_pkg_idx+=($i)
+          break
+      fi
+    done
+  done
+
+  # check if all packages found
+  
+  local found1=1
+  for name_needed in "${pkgs_to_install_names[@]}"
+  do
+    local found2=0
+    for name_found in "${found_pkg_names[@]}"
+    do
+      if [ "$name_needed" == "$name_found" ]; then
+        found2=1
+        print "Found package: $name_found"
+        break
+      fi
+    done
+    if [ $found2 -eq 0 ]; then
+      found1=0
+      print_error "Error: Did not find package: $name_needed"
+    fi
+  done
+  if [ $found1 -ne 1 ]; then
+    print_error "Error: Some packages are missing."
+    return 1
+  fi
+
+  print "All packages have been found in the repo."
+
+  # download needed packages
+
+  for idx in "${!found_pkg_names[@]}"
+  do
+    local name=${found_pkg_names[$idx]}
+    local pkg_idx=${found_pkg_idx[$idx]}
+    local url="$repo_root/${pkg_fnames[$pkg_idx]}"
+    local file="$SNIPS_DIR/$name.deb"
+    if [ $REUSE_DEB_FILES -eq 1 ] && [ -f "$file" ]; then
+      print "No need to download package $name because it exists."
+    else
+      print "Downloading package $name from $url."
+      wget --output-document="$file" "$url"
+      if [ $? -ne 0 ]; then
+        print_error "Error: Cannot download package $name from $url."
+        return 1
+      fi
+    fi
+  done
+
+  print "All deb packages were downloaded."
+
+  # extract deb packages
+
+  for name in "${found_pkg_names[@]}"
+  do
+    local deb_file="$SNIPS_DIR/$name.deb"
+    local dir="$SNIPS_DIR/deb-$name"
+    print "Extracting $deb_file to $dir dir."
+    set -e
+    mkdir -p "$dir"
+    cd "$dir"
+    set +e
+    ar x "$deb_file"
+    local error=$?
+    cd "$work_dir"
+    if [ $error -ne 0 ]; then
+      print_error "Error: Cannot extract $deb_file."
+      return 1
+    fi
+    local data_file="$dir/data.tar.xz"
+    print "Extracting $data_file to $dir dir."
+    tar -xf "$data_file" --directory "$dir"
+    if [ $error -ne 0 ]; then
+      print_error "Error: Cannot extract $data_file."
+      return 1
+    fi
+  done
+
+  print "All deb packages were extracted."
+}
+
+clean_pkgs() {
+  for name in "${pkgs_to_install_names[@]}"
+  do
+    local deb_file="$SNIPS_DIR/$name.deb"
+    local dir="$SNIPS_DIR/deb-$name"
+    if [ $KEEP_DEBS -eq 0 ] && [ -f "$deb_file" ]; then
+      print "Deleting file: $deb_file"
+      rm "$deb_file"
+    fi
+    if [ -d "$dir" ]; then
+      print "Deleting dir: $dir"
+      rm -R "$dir"
+    fi
+  done
+  pkgs_to_install_names=()
+  pkgs_to_install_vers=()
+}
+
+check_files() {
+  local error=0
+  for file in "${files_to_install[@]}"
+  do
+    if [ ! -f "$SNIPS_DIR/$(basename "$file")" ]; then
+      print_error "Error: File $file does not exist."
+      error=1
+    fi
+  done
+  files_to_install=()
+  return $error
+}
+
+# parse options
+
+while getopts ":Vkhrca:d:" options; do
+case "${options}" in
+  h)
+    usage
+    exit 0
+    ;;
+  V)
+    print "$VERSION"
+    exit 0
+    ;;
+  d)
+    SNIPS_DIR="${OPTARG}"
+    ;;
+  a)
+    ARCH=${OPTARG}
+    ;;
+  c)
+    CHECK_ONLY=1
+    ;;
+  k)
+    KEEP_DEBS=1
+    ;;
+  r)
+    REUSE_DEB_FILES=1
+    ;;
+  :)
+    exit_abnormal "Error: Option -$OPTARG requires an argument." 1
+    ;;
+  ?)
+    exit_abnormal "Error: Unknown option -$OPTARG." 1
+    ;;
+  esac
+done
+
+# check needed shell commands
+
+needed_commands=( wget sed ar tar gzip uname basename readlink dirname base64 )
+error=0
+for cmd in "${needed_commands[@]}"
+do
+  if ! [ -x "$(command -v $cmd)" ]; then
+    print_error "Error: $cmd is required but not installed."
+    error=1
+  fi
+done
+if [ $error -ne 0 ]; then
+  exit_abnormal
+fi
+
+# check if script is executed on SFOS ARM device
+
+cpu_arch=$(uname -p) # machine cpu arch
+
+if [ $cpu_arch == "aarch64" ]; then
+  sfos_arm=1
+else
+  sfos_arm=0
+fi
+
+# check arch and autodetect if not provided, valid arch is 'armhf', 'i386' or 'amd64'
+
+if [ "$ARCH" != "armhf" ] && [ "$ARCH" != "i386" ] && [ "$ARCH" != "amd64" ]; then
+  case "$cpu_arch" in
+    "aarch64")
+      ARCH="armhf"
+      ;;
+    "i686")
+      ARCH="i386"
+      ;;
+    "x86_64")
+      ARCH="amd64"
+      ;;
+    *)
+      if [ -z "$ARCH" ]; then
+        exit_abnormal "Error: Unknown architecture." 1
+      else
+        exit_abnormal "Error: Unknown architecture $ARCH." 1
+      fi
+  esac
+fi
+
+# check user dir, if not provided default to <snipek cache>/snips
+
+if [ -z "$SNIPS_DIR" ] && [ $sfos_arm -eq 1 ]; then
+  print "Using default directory for download: $SNIPS_DIR_DEFAULT."
+  if [ ! -d "$SNIPS_DIR_DEFAULT" ]; then
+    if [ $CHECK_ONLY -eq 1 ]; then
+      exit_abnormal "Error: Directory $SNIPS_DIR_DEFAULT does not exist." 1
+    fi
+    print "Creating $SNIPS_DIR_DEFAULT dir."
+    mkdir -p "$SNIPS_DIR_DEFAULT"
+    if [ $? -ne 0 ]; then
+      exit_abnormal "Error: Cannot create $SNIPS_DIR_DEFAULT dir."
+    fi
+  fi
+  SNIPS_DIR="$SNIPS_DIR_DEFAULT"
+elif [ ! -d "$SNIPS_DIR" ]; then
+  if [ -z "$SNIPS_DIR" ]; then
+    exit_abnormal "Error: Directory was not set." 1
+  else
+    exit_abnormal "Error: Directory $SNIPS_DIR does not exist." 1
+  fi
+else
+  SNIPS_DIR="$(readlink -f "$SNIPS_DIR")"
+fi
+
+# download binaries
+
+if [ $ARCH == "armhf" ]; then
+  lib_dir="arm-linux-gnueabihf"
+elif [ $ARCH == "i386" ]; then
+  lib_dir="i386-linux-gnu"
+elif [ $ARCH == "amd64" ]; then
+  lib_dir="amd64-linux-gnu"
+else
+  exit_abnormal "Error: Unknown architecture."
+fi
+
+# check only option
+
+if [ $CHECK_ONLY -eq 1 ]; then
+  print "Check if all needed files are present."
+  
+  files_to_install+=("$SNIPS_DIR/libsnips_kaldi.so")
+  files_to_install+=("$SNIPS_DIR/snips-asr")
+  files_to_install+=("$SNIPS_DIR/snips-dialogue")
+  files_to_install+=("$SNIPS_DIR/snips-hotword")
+  files_to_install+=("$SNIPS_DIR/snips-nlu")
+  files_to_install+=("$SNIPS_DIR/snips-tts")
+  
+  files_to_install+=("$SNIPS_DIR/libgfortran.so.3")
+  files_to_install+=("$SNIPS_DIR/mosquitto")
+  files_to_install+=("$SNIPS_DIR/libstdc++.so.6")
+  files_to_install+=("$SNIPS_DIR/libwrap.so.0")
+  files_to_install+=("$SNIPS_DIR/libcrypto.so.1.1")
+  files_to_install+=("$SNIPS_DIR/libssl.so.1.1")
+  files_to_install+=("$SNIPS_DIR/libwebsockets.so.8")
+  files_to_install+=("$SNIPS_DIR/libnsl.so.1")
+  files_to_install+=("$SNIPS_DIR/libev.so.4")
+  files_to_install+=("$SNIPS_DIR/libuv.so.1")
+  files_to_install+=("$SNIPS_DIR/libatlas.so.3")
+  files_to_install+=("$SNIPS_DIR/libcblas.so.3")
+  files_to_install+=("$SNIPS_DIR/libf77blas.so.3")
+  files_to_install+=("$SNIPS_DIR/liblapack_atlas.so.3")
+  
+  files_to_install+=("$SNIPS_DIR/libttspico.so.0")
+  files_to_install+=("$SNIPS_DIR/pico2wave")
+  
+  check_files
+  if [ $? -ne 0 ]; then
+    exit_abnormal "Error: Some binaries are missing."
+  fi
+  
+  if [ ! -f "$SNIPS_DIR/pico/lang/en-US_ta.bin" ]; then
+    exit_abnormal "Error: File $SNIPS_DIR/pico/lang/en-US_ta.bin does not exist."
+  fi
+  
+  files_to_install=()
+  print "Done. All needed files are present in $SNIPS_DIR dir."
+  exit 0
+fi
+
+print "Ready to start download for $ARCH cpu arch to $SNIPS_DIR directory."
+
+if [ $INSTALL_SNIPS -ne 0 ]; then  # Snips installation
+  print "Downloading Snips binaries..."
+
+  pkgs_to_install_names+=(snips-hotword)
+  pkgs_to_install_vers+=("0.64.0")
+  pkgs_to_install_names+=(snips-asr)
+  pkgs_to_install_vers+=("0.64.0")
+  pkgs_to_install_names+=(snips-nlu)
+  pkgs_to_install_vers+=("0.64.0")
+  pkgs_to_install_names+=(snips-tts)
+  pkgs_to_install_vers+=("0.64.0")
+  pkgs_to_install_names+=(snips-dialogue)
+  pkgs_to_install_vers+=("0.64.0")
+  pkgs_to_install_names+=(snips-platform-common)
+  pkgs_to_install_vers+=("0.64.0")
+  pkgs_to_install_names+=(snips-kaldi-atlas)
+  pkgs_to_install_vers+=("0.26.1")
+  
+  packages_url=$SNIPS_REPO_ROOT/dists/$SNIPS_DIST/main/binary-$ARCH/Packages.gz
+  download_and_extract_pkgs $SNIPS_REPO_ROOT $packages_url
+  if [ $? -ne 0 ]; then
+    exit_abnormal
+  fi
+  
+  files_to_install+=("$SNIPS_DIR/deb-snips-kaldi-atlas/usr/lib/libsnips_kaldi.so")
+  files_to_install+=("$SNIPS_DIR/deb-snips-asr/usr/bin/snips-asr")
+  files_to_install+=("$SNIPS_DIR/deb-snips-dialogue/usr/bin/snips-dialogue")
+  files_to_install+=("$SNIPS_DIR/deb-snips-hotword/usr/bin/snips-hotword")
+  files_to_install+=("$SNIPS_DIR/deb-snips-nlu/usr/bin/snips-nlu")
+  files_to_install+=("$SNIPS_DIR/deb-snips-tts/usr/bin/snips-tts")
+  for file in "${files_to_install[@]}"
+  do
+    cp --dereference "$file" "$SNIPS_DIR/"
+  done
+  
+  clean_pkgs
+  check_files
+  if [ $? -ne 0 ]; then
+    exit_abnormal "Error: Some binaries are missing."
+  fi
+  files_to_install=()
+fi
+
+if [ $INSTALL_DEBIAN_MAIN -ne 0 ]; then  # Debian main installation
+  print "Downloading Debian main binaries..."
+
+  pkgs_to_install_names+=(libgfortran3)
+  pkgs_to_install_vers+=("6.3.0-18")
+  pkgs_to_install_names+=(mosquitto)
+  pkgs_to_install_vers+=("1.4.10-3")
+  pkgs_to_install_names+=("libstdc++6")
+  pkgs_to_install_vers+=("6.3.0-18")
+  pkgs_to_install_names+=(libwrap0)
+  pkgs_to_install_vers+=("7.6.q-26")
+  pkgs_to_install_names+=(libssl1.1)
+  pkgs_to_install_vers+=("1.1.0k-1")
+  pkgs_to_install_names+=(libwebsockets8)
+  pkgs_to_install_vers+=("2.0.3-2")
+  pkgs_to_install_names+=(libc6)
+  pkgs_to_install_vers+=("2.24-11")
+  pkgs_to_install_names+=(libev4)
+  pkgs_to_install_vers+=("1:4.22-1")
+  pkgs_to_install_names+=(libuv1)
+  pkgs_to_install_vers+=("1.9.1-3")
+  pkgs_to_install_names+=(libatlas3-base)
+  pkgs_to_install_vers+=("3.10.3-1")
+  if [ $PATCH_PICO2WAVE -eq 1 ] && [ $ARCH == "armhf" ]; then  # needed to patch pico2wave
+    pkgs_to_install_names+=(bsdiff)
+    pkgs_to_install_vers+=("4.3-19")
+    pkgs_to_install_names+=("libbz2-1.0")
+    pkgs_to_install_vers+=("1.0.6-8.1")
+  fi
+  
+  packages_url=$DEBIAN_REPO_ROOT/dists/$DEBIAN_DIST/main/binary-$ARCH/Packages.gz
+  download_and_extract_pkgs $DEBIAN_REPO_ROOT $packages_url
+  if [ $? -ne 0 ]; then
+    exit_abnormal
+  fi
+
+  files_to_install+=("$SNIPS_DIR/deb-libgfortran3/usr/lib/$lib_dir/libgfortran.so.3")
+  files_to_install+=("$SNIPS_DIR/deb-mosquitto/usr/sbin/mosquitto")
+  files_to_install+=("$SNIPS_DIR/deb-libstdc++6/usr/lib/$lib_dir/libstdc++.so.6")
+  files_to_install+=("$SNIPS_DIR/deb-libwrap0/lib/$lib_dir/libwrap.so.0")
+  files_to_install+=("$SNIPS_DIR/deb-libssl1.1/usr/lib/$lib_dir/libcrypto.so.1.1")
+  files_to_install+=("$SNIPS_DIR/deb-libssl1.1/usr/lib/$lib_dir/libssl.so.1.1")
+  files_to_install+=("$SNIPS_DIR/deb-libwebsockets8/usr/lib/$lib_dir/libwebsockets.so.8")
+  files_to_install+=("$SNIPS_DIR/deb-libc6/lib/$lib_dir/libnsl.so.1")
+  files_to_install+=("$SNIPS_DIR/deb-libev4/usr/lib/$lib_dir/libev.so.4")
+  files_to_install+=("$SNIPS_DIR/deb-libuv1/usr/lib/$lib_dir/libuv.so.1")
+  files_to_install+=("$SNIPS_DIR/deb-libatlas3-base/usr/lib/libatlas.so.3")
+  files_to_install+=("$SNIPS_DIR/deb-libatlas3-base/usr/lib/libcblas.so.3")
+  files_to_install+=("$SNIPS_DIR/deb-libatlas3-base/usr/lib/libf77blas.so.3")
+  files_to_install+=("$SNIPS_DIR/deb-libatlas3-base/usr/lib/liblapack_atlas.so.3")
+  if [ $PATCH_PICO2WAVE -eq 1 ] && [ $ARCH == "armhf" ]; then
+    files_to_install+=("$SNIPS_DIR/deb-bsdiff/usr/bin/bspatch")
+    files_to_install+=("$SNIPS_DIR/deb-libbz2-1.0/lib/$lib_dir/libbz2.so.1.0")
+  fi
+
+  for file in "${files_to_install[@]}"
+  do
+    cp --dereference "$file" "$SNIPS_DIR/"
+  done
+
+  clean_pkgs
+  check_files
+  if [ $? -ne 0 ]; then
+    exit_abnormal "Error: Some binaries are missing."
+  fi
+  files_to_install=()
+fi
+
+if [ $INSTALL_DEBIAN_NONFREE -ne 0 ]; then  # Debian non-free installation
+  print "Downloading Debian non-free binaries..."
+
+  pkgs_to_install_names+=(libttspico0)
+  pkgs_to_install_vers+=("1.0")
+  pkgs_to_install_names+=(libttspico-utils)
+  pkgs_to_install_vers+=("1.0")
+  pkgs_to_install_names+=(libttspico-data)
+  pkgs_to_install_vers+=("1.0")
+
+  packages_url=$DEBIAN_REPO_ROOT/dists/$DEBIAN_DIST/non-free/binary-$ARCH/Packages.gz
+  download_and_extract_pkgs $DEBIAN_REPO_ROOT $packages_url
+  if [ $? -ne 0 ]; then
+    exit_abnormal
+  fi
+
+  files_to_install+=("$SNIPS_DIR/deb-libttspico0/usr/lib/$lib_dir/libttspico.so.0")
+  files_to_install+=("$SNIPS_DIR/deb-libttspico-utils/usr/bin/pico2wave")
+  for file in "${files_to_install[@]}"
+  do
+    cp --dereference "$file" "$SNIPS_DIR/"
+  done
+  cp -R $SNIPS_DIR/deb-libttspico-data/usr/share/pico $SNIPS_DIR/
+  
+  clean_pkgs
+  check_files
+  if [ $? -ne 0 ]; then
+    exit_abnormal "Error: Some binaries are missing."
+  fi
+  if [ ! -f "$SNIPS_DIR/pico/lang/en-US_ta.bin" ]; then
+    exit_abnormal "Error: File $SNIPS_DIR/pico/lang/en-US_ta.bin does not exist."
+  fi
+  files_to_install=()
+fi
+
+if [ $INSTALL_ASSISTANT -ne 0 ]; then  # Assistant installation
+  print "Downloading assistant file..."
+  download_and_extract_assistant
+  if [ $? -ne 0 ]; then
+    exit_abnormal
+  fi
+fi
+
+# Patch pico2wave only for armhf because only armhf patch is available
+
+if [ $PATCH_PICO2WAVE -eq 1 ] && [ $ARCH == "armhf" ]; then
+  print "Applying patch on pico2wave."
+  
+  pico2wave_file="$SNIPS_DIR/pico2wave"
+  pico2wave_patched="$SNIPS_DIR/pico2wave_patched"
+  pico2wave_patch_file="$SNIPS_DIR/pico2wave.patch"
+  bspatch_cmd="bspatch"
+
+  if [ $sfos_arm -eq 1 ]; then # script executed on arm device, so using own bspatch
+    bspatch_cmd="$SNIPS_DIR/bspatch"
+    if ! [ -x "$(command -v "$bspatch_cmd")" ]; then
+      exit_abnormal "Error: Cannot patch pico2wave because $bspatch_cmd is required but not available."
+    fi
+  fi
+  
+  echo "$PICO2WAVE_PATCH" | base64 -d -- > $pico2wave_patch_file
+  if [ $? -ne 0 ]; then
+    exit_abnormal "Error: Cannot create pico2wave patch file."
+  fi
+  
+  if [ $sfos_arm -eq 1 ]; then
+    export LD_LIBRARY_PATH=$SNIPS_DIR
+  fi
+  
+  $bspatch_cmd $pico2wave_file $pico2wave_patched $pico2wave_patch_file
+  if [ $? -ne 0 ]; then
+    rm $pico2wave_patch_file
+    exit_abnormal "Error: Cannot apply patch on pico2wave."
+  fi
+  
+  if [ $sfos_arm -eq 1 ]; then
+    export -n LD_LIBRARY_PATH
+  fi
+
+  rm $pico2wave_patch_file
+  rm $pico2wave_file
+  mv $pico2wave_patched $pico2wave_file
+  chmod a+x $pico2wave_file
+  if [ $? -ne 0 ] || [ ! -f $pico2wave_file ]; then
+    exit_abnormal "Error: Cannot apply patch on pico2wave."
+  fi
+  
+  print "Pico2wave patched successfully."
+fi
+
+print "Done. All files were successfully downloaded to $SNIPS_DIR."
---
+++ sailfish/snips/snips_start.sh
@@ -0,0 +1,259 @@
+#!/usr/bin/env bash
+
+# Script that starts essential Snips components on Sailfish OS
+# Copyright (c) Michal Kosciesza <michal@mkiol.net>
+
+VERSION=1.0.0
+
+SNIPS_DIR_DEFAULT=/home/nemo/.cache/harbour-snipek/harbour-snipek/snips
+SNIPS_DIR=$SNIPS_DIR_DEFAULT
+
+########################
+
+STOP=0
+VERBOSE=0
+CHECK_RUNNING=0
+
+print() {
+  echo "$1"
+}
+
+print_error() {
+  echo "$1" 1>&2
+}
+
+usage() {
+  print "Usage: $0 [OPTION]..."
+  print "Start Snips processes."
+  print ""
+  print "Options:"
+  print "  -d <DIR>     directory where Snips is installed (default is $SNIPS_DIR_DEFAULT)"
+  print "  -k           stop instead start"
+  print "  -c           check if Snips is already running instead start"
+  print "  -v           sets the verbosity"
+  print "  -h           display this help and exit"
+  print "  -V           output version information and exit"
+}
+
+exit_abnormal() {
+  local txt="$1"
+  local show_usage="$2"
+  if [ -n "$txt" ]; then
+    print_error "$1"
+  fi
+  if [ -n "$show_usage" ]; then
+    usage
+  fi
+  exit 1
+}
+
+check_processes() {
+  local processes=($(ps -e | sed -r 's/.*[0-9][0-9]:[0-9][0-9]:[0-9][0-9] ([^ ]+).*$/\1/g;t;d'))
+  local error=0
+  for needed_process in "${snips_processes[@]}"
+  do
+    local found=0
+    for process in "${processes[@]}"
+    do
+      if [ "$process" == "$needed_process" ]; then
+        print "Process $process is running."
+        found=1
+        break
+      fi
+    done
+    if [ $found -eq 0 ]; then
+      print_error "Process $needed_process is not running."
+      error=1
+    fi
+  done
+  return $error
+}
+
+kill_processes() {
+  for process in "${snips_processes[@]}"
+  do
+    killall -9 "$process"
+    if [ $? -eq 0 ]; then
+      print "Process $process stopped."
+    fi
+  done
+}
+
+check_all_files() {
+  local files_to_install=()
+  files_to_install+=("$SNIPS_DIR/libsnips_kaldi.so")
+  files_to_install+=("$SNIPS_DIR/snips-asr")
+  files_to_install+=("$SNIPS_DIR/snips-dialogue")
+  files_to_install+=("$SNIPS_DIR/snips-hotword")
+  files_to_install+=("$SNIPS_DIR/snips-nlu")
+  files_to_install+=("$SNIPS_DIR/snips-tts")
+  
+  files_to_install+=("$SNIPS_DIR/libgfortran.so.3")
+  files_to_install+=("$SNIPS_DIR/mosquitto")
+  files_to_install+=("$SNIPS_DIR/libstdc++.so.6")
+  files_to_install+=("$SNIPS_DIR/libwrap.so.0")
+  files_to_install+=("$SNIPS_DIR/libcrypto.so.1.1")
+  files_to_install+=("$SNIPS_DIR/libssl.so.1.1")
+  files_to_install+=("$SNIPS_DIR/libwebsockets.so.8")
+  files_to_install+=("$SNIPS_DIR/libnsl.so.1")
+  files_to_install+=("$SNIPS_DIR/libev.so.4")
+  files_to_install+=("$SNIPS_DIR/libuv.so.1")
+  files_to_install+=("$SNIPS_DIR/libatlas.so.3")
+  files_to_install+=("$SNIPS_DIR/libcblas.so.3")
+  files_to_install+=("$SNIPS_DIR/libf77blas.so.3")
+  files_to_install+=("$SNIPS_DIR/liblapack_atlas.so.3")
+  
+  files_to_install+=("$SNIPS_DIR/libttspico.so.0")
+  files_to_install+=("$SNIPS_DIR/pico2wave")
+  
+  local error=0
+  for file in "${files_to_install[@]}"
+  do
+    if [ ! -f "$file" ]; then
+      print_error "Error: File $file does not exist."
+      error=1
+    fi
+  done
+  
+  if [ ! -f "$SNIPS_DIR/pico/lang/en-US_ta.bin" ]; then
+    print_error "Error: File $SNIPS_DIR/pico/lang/en-US_ta.bin does not exist."
+    error=1
+  fi
+  
+  return $error
+}
+
+# parse options
+
+while getopts ":Vhcvkd:" options; do
+case "${options}" in
+  h)
+    usage
+    exit 0
+    ;;
+  V)
+    print "$VERSION"
+    exit 0
+    ;;
+  d)
+    SNIPS_DIR="${OPTARG}"
+    ;;
+  k)
+    STOP=1
+    ;;
+  c)
+    CHECK_RUNNING=1
+    ;;
+  v)
+    VERBOSE=1
+    ;;
+  :)
+    exit_abnormal "Error: Option -$OPTARG requires an argument." 1
+    ;;
+  ?)
+    exit_abnormal "Error: Unknown option -$OPTARG." 1
+    ;;
+  esac
+done
+
+# check needed shell commands
+
+needed_commands=( killall readlink )
+error=0
+for cmd in "${needed_commands[@]}"
+do
+  if ! [ -x "$(command -v $cmd)" ]; then
+    print_error "Error: $cmd is required but not installed."
+    error=1
+  fi
+done
+if [ $error -ne 0 ]; then
+  exit_abnormal
+fi
+
+# check snips dir, if not provided default to <snipek cache>/snips
+
+if [ ! -d "$SNIPS_DIR" ]; then
+  exit_abnormal "Error: Directory $SNIPS_DIR does not exist." 1
+else
+  SNIPS_DIR=$(readlink -f "$SNIPS_DIR")
+fi
+
+print "Using directory: $SNIPS_DIR."
+
+declare -a snips_processes
+snips_processes+=("snips-asr")
+snips_processes+=("snips-tts")
+snips_processes+=("snips-dialogue")
+snips_processes+=("snips-nlu")
+snips_processes+=("snips-hotword")
+snips_processes+=("snips-asr")
+snips_processes+=("mosquitto")
+
+# check if Snips is running
+
+if [ $CHECK_RUNNING -eq 1 ]; then
+  print "Check if all Snips processes are running."
+  check_processes
+  if [ $? -ne 0 ]; then
+    exit_abnormal "Error: Some Snips processes are stopped."
+  fi
+  print "Done. All Snips processes are running."
+  exit 0
+fi
+
+# export required viariables
+
+export LD_LIBRARY_PATH=$SNIPS_DIR
+export PATH=$PATH:$SNIPS_DIR
+
+# stop
+
+print "Stopping Snips processes..."
+kill_processes
+
+if [ $STOP -eq 0 ]; then
+  print "Check if all needed files are present in $SNIPS_DIR directory."
+  check_all_files
+  if [ $? -ne 0 ]; then
+    exit_abnormal "Error: Some needed files are missing."
+  fi
+
+  print "Starting Snips..."
+  
+  $SNIPS_DIR/mosquitto -d
+  if [ $? -ne 0 ]; then
+    exit_abnormal "Error: Cannot start MQTT."
+  fi
+  
+  assistant_dir=$SNIPS_DIR/assistant
+  pushd $SNIPS_DIR
+  if [ $VERBOSE -eq 1 ]; then
+    opts="-v"
+    $SNIPS_DIR/snips-hotword $opts --assistant $assistant_dir --user-dir $SNIPS_DIR &
+    $SNIPS_DIR/snips-nlu $opts --assistant $assistant_dir --user-dir $SNIPS_DIR &
+    $SNIPS_DIR/snips-dialogue $opts --assistant $assistant_dir --user-dir $SNIPS_DIR &
+    $SNIPS_DIR/snips-tts $opts --assistant $assistant_dir --user-dir $SNIPS_DIR --provider picotts &
+    $SNIPS_DIR/snips-asr $opts --assistant $assistant_dir --user-dir $SNIPS_DIR &
+  else
+    $SNIPS_DIR/snips-hotword --assistant $assistant_dir --user-dir $SNIPS_DIR > /dev/null 2>&1 &
+    $SNIPS_DIR/snips-nlu --assistant $assistant_dir --user-dir $SNIPS_DIR > /dev/null 2>&1 &
+    $SNIPS_DIR/snips-dialogue --assistant $assistant_dir --user-dir $SNIPS_DIR > /dev/null 2>&1 &
+    $SNIPS_DIR/snips-tts --assistant $assistant_dir --user-dir $SNIPS_DIR --provider picotts > /dev/null 2>&1 &
+    $SNIPS_DIR/snips-asr --assistant $assistant_dir --user-dir $SNIPS_DIR > /dev/null 2>&1 &
+  fi
+  popd
+  
+  print "Check if all Snips processes are running."
+  check_processes
+  if [ $? -ne 0 ]; then
+    print_error "Error: Snips cannot be started."
+    kill_processes
+    exit_abnormal
+  fi
+  
+  print "Done. Snips successfuly started."
+else
+  print "Done. Snips successfuly stopped."
+fi
+
